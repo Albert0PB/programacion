e4b08e9ee251d33c1698aca00801ccb781e6fe37
Albert0PB <a23pebeal@iesgrancapitan.org>
2024-03-25 16:36:59 +0100

    añade ejercicios de excepciones

---
+++ excepciones/ej1.py
@@ -0,0 +1,5 @@
+"""
+1. Modifica tu clase para gestionar menús de opciones en consola de forma que cuando el usuario introduzca 
+una opción no entera se capture la excepción, se le advierta de que solo se admiten valores numéricos y se pida 
+de nuevo la opción.
+"""
---
+++ excepciones/ej2.py
@@ -0,0 +1,4 @@
+"""
+2. Modifica los ejercicios de la clase para cuentas bancarias y de manejo de cartas de manera que las excepciones que se lanzan sean personalizadas.
+Author: Alberto Pérez Bernabeu
+"""
